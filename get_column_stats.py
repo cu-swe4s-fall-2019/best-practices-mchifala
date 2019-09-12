5fc2226c1ff2191499e2f4b3542aff78f417fe85
mchifala <mich5456@colorado.edu>
2019-09-12 21:52:29 +0000

    Add argparse, main function, error handling, comments, docstrings, and PEP8 style best practices to file

--- get_column_stats.py
+++ get_column_stats.py
@@ -1,19 +1,85 @@
-import sys, math
+import sys
+import math
+import argparse
 
-file_name = sys.argv[1]
-col_num = int(sys.argv[2])
 
-f = open(file_name, 'r')
+def main(file_name, col_num):
+    """
+    This script calculates the mean and standard deviation of a column in a data file.
 
-V = []
+    Parameters:
+    ___________
+        - file_name(str): The file of interest
+        - col_num(int): The specific column we want to analyze
 
-for l in f:
-    A = [int(x) for x in l.split()]
-    V.append(A[col_num])
+    Returns:
+    ________
+        - None; the mean and standard deviation are printed to stdout
+    """
 
-mean = sum(V)/len(V)
+    # Open user-defined file
+    try:
+        f = open(file_name, 'r')
 
-stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+    except FileNotFoundError:
+        print("Data file not found. Please check file name and directory.")
+        sys.exit(1)
 
-print('mean:', mean)
-print('stdev:', stdev)
+    except PermissionError:
+        print("Invalid permissions for data file.")
+        sys.exit(1)
+
+    V = []
+
+    # Append each element of the user-defined column to a list
+    try:
+        for l in f:
+            A = [int(x) for x in l.split()]
+            V.append(A[col_num])
+
+    except ValueError:
+        print("Data file contains a value with invalid input type.")
+        sys.exit(1)
+
+    except IndexError:
+        print("Column number does not exist in data file.")
+        sys.exit(1)
+
+    # Calculate mean of the elements in list
+    try:
+        mean = sum(V)/len(V)
+
+    except ZeroDivisionError:
+        print("Column is empty. Cannot perform calculations")
+        sys.exit(1)
+
+    # Calculate standard deviation of the elements in list
+    try:
+        stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+
+    except ZeroDivisionError:
+        print("Column is empty. Cannot perform calculations")
+        sys.exit(1)
+
+    # Print results
+    print('mean:', mean)
+    print('stdev:', stdev)
+
+    return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Column stats input")
+
+    parser.add_argument('--file_name',
+                        type=str,
+                        help='Name of the file (with extension)',
+                        required=True)
+
+    parser.add_argument('--col_num',
+                        type=int,
+                        help='Column number to be analyzed',
+                        required=True)
+
+    args = parser.parse_args()
+    main(args.file_name, args.col_num)
