13d6586a9e2587c47deb91e59f3c150ada8e9b6b
mchifala <mich5456@colorado.edu>
2019-09-12 21:53:42 +0000

    Add additional tests to bash script and configure for use with command line arguments

--- basics_test.sh
+++ basics_test.sh
@@ -2,16 +2,39 @@ pycodestyle style.py
 
 pycodestyle get_column_stats.py
 
+# Test for random inputs
 (for i in `seq 1 100`; do 
     echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
 done )> data.txt
+python get_column_stats.py --file_name data.txt --col_num 2
 
-python get_column_stats.py data.txt 2
-
-
+# Test for static input
 V=1
 (for i in `seq 1 100`; do 
     echo -e "$V\t$V\t$V\t$V\t$V";
 done )> data.txt
+python get_column_stats.py --file_name data.txt --col_num 2
 
-python get_column_stats.py data.txt 2
+# Test error handling for non-existent column
+(for i in `seq 1 100`; do
+    echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
+done )> data.txt
+python get_column_stats.py --file_name data.txt --col_num 666
+
+# Test error handling for invalid column input
+(for i in `seq 1 100`; do
+    echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\tA";
+done )> data.txt
+python get_column_stats.py --file_name data.txt --col_num 2
+
+# Test error handling for bad file path
+(for i in `seq 1 100`; do
+    echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
+done )> data.txt
+python get_column_stats.py --file_name bad_file.txt --col_num 2
+
+# Test error handling for zero division
+(for i in `seq 1 0`; do
+    echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
+done )> data.txt
+python get_column_stats.py --file_name data.txt --col_num 2
